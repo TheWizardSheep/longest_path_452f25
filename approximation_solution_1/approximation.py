2420fe7879fabe9c99106a35f606a05e6c977f5c
Will <64212425+TheWizardSheep@users.noreply.github.com>
2025-11-21 15:10:51 -0500

    fixed code. Done with approx, need tests

--- approximation_solution_1/approximation.py
+++ approximation_solution_1/approximation.py
@@ -1,53 +1,50 @@
 import networkx
 
 def approximation(text):
-    # Example LP input:
-    # 3 3
-    # a b 3
-    # b c 4
-    # a c 5
     g = networkx.Graph()
-    #Handle Input
-    lines = text.split("\n")
-    v, e = map(int, lines[0].split(" "))
+    
+    # Handle Input
+    lines = text.strip().split("\n")
+    n, e = map(int, lines[0].split())
     for i in range(e):
-        u, v, w = lines[i+1].split()
+        u, v, w = lines[i + 1].split()
         g.add_edge(u, v, weight=int(w))
 
-    #Reduce to max spanning tree
-    edgesSorted = sorted(g.edges(data=True), key=lambda x: x[2].get('weight'), reverse=True)
+    # Reduce to max spanning tree
+    edgesSorted = sorted(g.edges(data=True), key=lambda x: x[2]['weight'], reverse=True)
     mst = networkx.Graph()
-    for u, v, attr in edgesSorted: #! failure todo
-        try: 
-            if networkx.has_path(mst, e[0], e[1]):
-                continue
-            else:
-                mst.add_edge(e[0],e[1], e[2].get('weight'))
-        except networkx.exception.NodeNotFound as e:
-            mst.add_edge(e[0], e[1], e[2].get('weight'))
+    for u, v, attr in edgesSorted:
+        try:
+            if not networkx.has_path(mst, u, v):
+                mst.add_edge(u, v, weight=attr['weight'])
+        except networkx.NodeNotFound:
+            mst.add_edge(u, v, weight=attr['weight'])
             continue
-    print(mst.edges)
-    
-    #DFS
+
+    # DFS to find heaviest path
     def dfs(curr, curr_path, curr_weight):
         best = (curr_weight, curr_path)
         for neighbor, attr in mst[curr].items():
-            if not(curr_path.includes(neighbor)):
-                w = attr.get("weight")
-                candidate = dfs(neighbor, curr_weight + w, curr_path + [neighbor])
+            if neighbor not in curr_path:
+                w = attr['weight']
+                candidate = dfs(neighbor, curr_path + [neighbor], curr_weight + w)
                 if candidate[0] > best[0]:
                     best = candidate
         return best
-    
-    best = (-1000, '')
+
+    best = (-float('inf'), [])
     for node in mst.nodes:
-        weight, path = dfs(node, [], 0)
-        best = (weight, path) if weight > best[0] else best
+        weight, path = dfs(node, [node], 0)
+        if weight > best[0]:
+            best = (weight, path)
 
     return best
 
 
 if __name__ == "__main__":
-    text = """3 3\na b 3\nb c 4\na c 5"""
+    text = """3 3
+a b 3
+b c 4
+a c 5"""
     ans = approximation(text)
     print(ans[1], ans[0])
